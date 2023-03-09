1f3fe34fd2d58e7d2d73838f3bd120dad9effd99
framognino <federico.ramognino@polimi.it>
2023-03-09 12:21:56 +0100

    Removed tests from repository

--- tests/laminarFlameSpeed.py
+++
@@ -1,9 +0,0 @@
-import sys
-# setting path
-sys.path.append('../')
-
-from src.thermophysicalModels.laminarFlameSpeed.tabulatedLFS.tabulatedLFS import tabulatedLFS
-
-tab = tabulatedLFS.fromFile("./Table")
-
-print(tab.tables["Su"].__dict__)
