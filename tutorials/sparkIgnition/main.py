9e29e9da793b16fac046b0caac0fc5f5f4a4d9f9
Balle <alberto.ballerini97@gmail.com>
2025-02-11 11:32:04 +0100

    Changes to main

--- tutorials/sparkIgnition/main.py
+++ tutorials/sparkIgnition/main.py
@@ -7,8 +7,13 @@ Testing solver for post-processing of CFD results from spark-ignition simulation
 #Importing
 import os
 import traceback
+import shutil
 from libICEpost.src.engineModel.EngineModel.EngineModel import EngineModel
 from libICEpost.src.base.dataStructures.Dictionary import Dictionary
+
+#To disable the warning from the janaf7 module of out of bound temperature
+#from libICEpost.src.thermophysicalModels.specie.thermo.Thermo.janaf7 import janaf7
+#janaf7.__WARNING__ = False
 
 #Define function for simpify loading:
 def loadModel(path:str, controlDictName:str="controlDict.py") -> EngineModel|None:
@@ -23,11 +28,11 @@ def loadModel(path:str, controlDictName:str="controlDict.py") -> EngineModel|Non
     """
     
     try:
-        # Load the constrol dictionary
+        #Load the constrol dictionary
         print(f"Loading engine model from {path}/{controlDictName}")
         controlDict = Dictionary.fromFile(f"{path}/{controlDictName}")
         
-        # Lookup engine model type and its dictionary
+        #Lookup engine model type and its dictionary
         engineModelType:str = controlDict.lookup("engineModelType")
         engineModelDict:Dictionary = controlDict.lookup(engineModelType + "Dict")
         
@@ -58,6 +63,8 @@ model.process()
 from matplotlib import pyplot as plt
 
 #Setting plotting parameters
+plt.rcParams.setdefault
+
 plt.rcParams.update({
     # "text.usetex": True,
     # "font.family": "serif",
@@ -70,20 +77,49 @@ plt.rcParams.update({
     'lines.linewidth': 3,
     'axes.linewidth': 2,
     'figure.figsize':(6,6),
-    # 'figure.dpi':10
+    #'figure.dpi':10
 })
+
+#Example of plotting and general output
 #Plotting pressure-ROHR
 axP, axRohr = model.plotP_ROHR(label="CFD", legend=True, c="k", figsize=(6,8))
 
 #Adjust axes, limits, legend
-plt.tight_layout()
 axP.set_xlim((-30,30))  #x axis
 axP.set_ylim((-10,40))  #pressure
 axRohr.set_ylim((0,100)) #ROHR
 axP.legend(loc="upper left")
+plt.tight_layout()
 
 #Plotting p-V diagram
 ax = model.plotPV(label="CFD", timingsParams={"markersize":120, "linewidth":2})
+plt.tight_layout()
 
-#Show the figures
+#Output of the data to a csv file with comma as separator
+#Check if the "output" folder exists, if not it creates it
+#If it already exists, it removes it and creates a new one
+output_folder = "output"
+if os.path.exists(output_folder) and os.path.isdir(output_folder):
+    # Remove the "output" folder
+    shutil.rmtree(output_folder)
+    print(f"Removed folder: {output_folder}")
+    os.mkdir(output_folder)
+    print(f"Created folder: {output_folder}")
+else:
+    print(f"Folder does not exist: {output_folder}")
+    os.mkdir(output_folder)
+    print(f"Created folder: {output_folder}")
+    
+model.data[:].to_csv(output_folder + "/Complete_processed_data.csv", sep=',', index=False)
+
+#Save the pressure-ROHR plot
+pressure_rohr_fig = axP.get_figure()
+pressure_rohr_fig.savefig(output_folder + "/pressure_rohr_plot.png")
+
+#Save the p-V diagram plot
+pv_fig = ax.get_figure()
+pv_fig.savefig(output_folder + "/pv_plot.png")
+
+#Show the plots in matplotlib windows, note that this will not end the script
+#until all matplotlib windows are closed
 plt.show()
