6c0e939dcb0fd40c20c516ad6a3083056a6ffd6a
framognino <federico.ramognino@polimi.it>
2025-04-07 18:53:36 +0200

    test_imports: added modules to skip when testing all imports (skipping PyFoam)

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -9,13 +9,24 @@ def get_modules():
     baseModule = "./src/libICEpost"
     modules:list[str] = []
     
+    skip:list[str] = \
+        [
+            "./src/libICEpost/src/_utils/PyFoam"
+        ]
+    
     if not os.path.exists(baseModule):
         raise FileNotFoundError(f"Base module {baseModule} not found")
     
     #Walk the base folder, save all the folders containing a __init__.py file or even a .py files that are not __init__.py
     # Store the modelule name as its full path replacing the separator with a dot
     for root, dirs, files in os.walk(baseModule):
+        # Skip the folders that are in the skip list
+        if any([root.startswith(s) for s in skip]):
+            continue
         for file in files:
+            #Skip files that are in skip list
+            if os.path.join(root, file) in skip:
+                continue
             if file.endswith(".py") and not file == "__init__.py":
                 modules.append(os.path.join(root, file).replace(os.sep, ".").replace(".py", ""))
             elif file == "__init__.py":
